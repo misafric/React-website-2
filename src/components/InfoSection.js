9fa62053c158539c1c35f63b684f95d22fee2ac2
misafric <fricovam4@gmail.com>
2021-03-06 12:18:38 +0100

    infosection

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -2,23 +2,76 @@ import React from 'react'
 import styled from 'styled-components';
 import { Button } from './Button';
 
-const Section = styled.section``;
-const Container = styled.div``;
-const ColumnLeft = styled.div``;
-const ColumnRight = styled.div``;
+const Section = styled.section`
+    height: 100%;
+    width: 100%;
+    padding: 4rem 0rem;
+`;
 
-const InfoSection = () => {
+const Container = styled.div`
+    padding: 3rem calc((100vw - 1200px) / 2);
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 800px;
+
+    @media screen and (max-width: 768px) {
+        grid-template-columns: 1fr;
+    }
+`;
+
+const ColumnLeft = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    line-height: 1.4;
+    padding: 1rem 2rem;
+    order: ${({ reverse }) => (reverse ? '2' : '1')};
+
+    h1 {
+        margin-bottom: 1rem;
+        font-size: clamp(1.5rem, 5vw, 2rem);
+    }
+
+    p {
+        margin-bottom: 2rem;
+    }
+`;
+
+const ColumnRight = styled.div`
+    padding: 1rem 2rem;
+    order: ${({ reverse }) => (reverse ? '1' : '2')};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    @media screen and (max-width: 768px) {
+        order: ${({ reverse }) => (reverse ? '2' : '1')};
+    }
+
+    img {
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+
+        @media screen and (max-width: 768px) {
+            height: 90%;
+            width: 90%;
+        }
+`;
+
+const InfoSection = ({heading, paragraphOne, paragraphTwo, buttonLabel, reverse, image}) => {
     return (
         <Section>
             <Container>
                 <ColumnLeft>
-                    <h1>head</h1>
-                    <p>para</p>
-                    <p>para</p>
-                    <Button to='/homes'>label</Button>
+                    <h1>{heading}</h1>
+                    <p>{paragraphOne}</p>
+                    <p>{paragraphTwo}</p>
+                    <Button to='/homes' primary='true'>{buttonLabel}</Button>
                 </ColumnLeft>
-                <ColumnRight>
-                    <img src='' alt='/home' />
+                <ColumnRight reverse={reverse}>
+                    <img src={image} alt='/home' />
                 </ColumnRight>
             </Container>
         </Section>
---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImgOne from '../images/InteriorVertical.jpg';
+import ImgTwo from '../images/HouseVertical.jpg';
+
+export const InfoData = {
+    heading: 'Discover your dream home!',
+    paragraphOne: 'We have a wide range of mansions, houses and villas from all over the world. Explore your options and pick the the best fit for you. The house you have been always dreaming about is within a reach.',
+    paragraphTwo: 'Trust in your ability to fulfill your dream. Get out of your comfort zone. Take risks, and follow your passion day in and day out.',
+    buttonLabel: 'See the homes',
+    image: ImgOne,
+    reverse: false,
+    delay: 100
+}
+
+export const InfoDataTwo = {
+    heading: 'Discover your dream home!',
+    paragraphOne: 'We have a wide range of mansions, houses and villas from all over the world. Explore your options and pick the the best fit for you. The house you have been always dreaming about is within a reach.',
+    paragraphTwo: 'Trust in your ability to fulfill your dream. Get out of your comfort zone. Take risks, and follow your passion day in and day out.',
+    buttonLabel: 'See the homes',
+    image: ImgTwo,
+    reverse: true,
+    delay: 300
+}
